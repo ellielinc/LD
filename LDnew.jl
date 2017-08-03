3709682443cba274a801ec8d06dc1ed71e1ec7af
ellielinc <ellielinc@gmail.com>
2017-08-03 17:25:26 -0400

    LD current
    Current methods  in calculating LD

--- LDnew.jl
+++ LDnew.jl
@@ -1,49 +1,48 @@
 using PyPlot
 include("oiplot.jl")
+include("readoifits.jl")
+include("setupft.jl")
+include("oichi2.jl")
+include("vfunc.jl")
 
-inty = zeros(1024,1024)
-center = [1025/2, 1025/2]
-nx = 1024
-#x = reshape([(div(i-1,nx)+1) for i=1:nx*nx], nx, nx)-(nx+1)/2
-#y = reshape([(div(i-1,nx)+1) for i=1:nx*nx], nx, nx)-(nx+1)/2
-r = linspace(0, 300, 20)
-t = linspace(0, 6.283185307179586, 20)
-x = zeros(length(t)*length(r));
-for i = 1:length(t)
-  for u = 1:length(r)
-    j = t[i]
-  x[u*i] = r[u].*cos(j)
-  end
-end
+nx = 256
+#tic();
+y = reshape([(div(i-1,nx)+1) for i=1:nx*nx], nx, nx)-(nx+1)/2;
+x = reshape([(mod(i-1,nx)+1) for i=1:nx*nx], nx, nx)-(nx+1)/2;
+#toc();
 
-y = zeros(length(t)*length(r));
-for i = 1:length(t)
-  for u = 1:length(r)
-  j = t[i]
-  y[u*i] = r[u].*sin(j)
-  end
-end
+r=sqrt(x.^2+y.^2);
+rstar= 100;
+disc_indx = find(r.<=rstar);
+mu = zeros(size(r));
+mu[disc_indx] = sqrt(1-(r[disc_indx]/rstar).^2);
+alpha = 0.89;
+f = mu.^alpha;
+flux = vec(f);
+imdisp(flux)
+#savefig("LDsamp.jpg")
+# step 2
+image = copy(flux);
+pixellation = 0.01; # in mas/pixel
+scale_rad = pixellation * (pi / 180.0) / 3600000.0;
 
-for i=1:length(x)
-    for j=1:length(y)
-    r=sqrt(x[i].^2+y[j].^2)
-  if r>=200
-  x[i]=0
-  y[j]=0
-  else
-  x[i]=1
-  y[j]=1
-  end
-end
-end
+#how to save img as oifits
+# open data file
+oifitsfile = "2004-data1.oifits";
+data = read_oifits(oifitsfile);
 
-#=
-mu = sqrt(1-(r./rstar).^2)
-alpha = 0.89
-f = mu.^alpha
-flux = vec(r)
-#imdisp(flux)
-#theta = atan(x./y)
-#scatter(r, theta)
-#make into a circle
-=#
+#SATLAS?
+
+# setup Fourier transform matrix
+dft = setup_ft(data, nx);
+# compute Fourier transform of the image
+cvis_image = image_to_cvis(image, dft)
+
+u=data.uv[1,:];
+v=data.uv[2,:];
+ruv=sqrt(u.^2+v.^2);
+cvis_analytic = visibility_ldpow([2.0, 0.89], ruv)
+v2_model = cvis_to_v2(cvis_analytic, data.indx_v2);
+v2_model2 = cvis_to_v2(cvis_image, data.indx_v2);
+opt = maximum(abs(v2_model-v2_model2)./abs(v2_model))*100.
+println("maximum = $opt")
