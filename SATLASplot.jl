d48f0a34849dc9660ba37ff95172c86d4525a17d
ellielinc <ellielinc@gmail.com>
2017-08-04 15:32:16 -0400

    unfinished SATLAS analysis
    Needs: FT of UD + rings

--- SATLASplot.jl
+++ SATLASplot.jl
@@ -1,32 +1,23 @@
 using RDatasets, DataFrames
 using PyPlot
+include("setupft.jl")
+include("oichi2.jl")
 include("oiplot.jl")
+include("vfunc.jl")
+include("hankel.jl")
+#include("readoifits.jl")
+sat1 = readtable("satlas_rsg/ld_satlas_surface.2t3300g-25m25", header = false, separator = ' ', nastrings=["NA", "na", "n/a", "missing"]);
+names!(sat1, [Symbol("col$i") for i in 1:9]);
+rename!(sat1, [:col1, :col6], [:mu, :flux])
+fI = sat1[:6];
+mu = sat1[:1];
+rstar = 5;
+r = rstar*(1-mu.^2);
+f = diff(fI);
+ra = diff(r);
+a = f./ra;
+b = fI[1:999].-(a.*r[1:999]);
+a.*(hankel(x)) + b.*(visibility_ud(param, v_r))
 
-satfile = readtable("satlas_rsg/ld_satlas_surface.2t3300g-25m25", header = false, separator = ' ', nastrings=["NA", "na", "n/a", "missing"]);
-names!(satfile, [Symbol("col$i") for i in 1:9]);
-rename!(satfile, [:col1, :col6], [:u, :v]);
-fI = satfile[:6];
-m = satfile[:1];
-rstar = 20;
-r = rstar*(1-m.^2);
-#t = linspace(0, 6.283185307179586, 190)
-t = linspace(0, 1.5707963267948966, 135)
-x = zeros(length(t)*length(r));
-for i = 1:length(t)
-  for u = 1:length(r)
-    j = t[i]
-  x[u*i] = r[u].*cos(j)
-  end
-end
 
-y = zeros(length(t)*length(r));
-for i = 1:length(t)
-  for u = 1:length(r)
-  j = t[i]
-  y[u*i] = r[u].*sin(j)
-  end
-end
-scatter(x, y)
-r = sqrt(x.^2+y.^2);
-rstar = 20;
-#mu = sqrt(1-(r/rstar).^2)
+#vis = (a.*(hankel(x)- b.*visibility_ud(param, v_r))
